aad60945dd14903a02cc15d68676923859646a77
Dominik Pokrzywa <83984820+Serrwo@users.noreply.github.com>
2024-05-24 15:38:44 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+extern void encrypt(unsigned int d, unsigned int n);
+extern void decrypt(unsigned int e, unsigned int n);
+
 void write_public_key(unsigned int e, unsigned int n) {
     FILE *fptr;
 
@@ -26,4 +29,63 @@ void write_private_key(unsigned int d, unsigned int n) {
 
     fprintf(fptr, "%d,%d", d, n);
     fclose(fptr);
+}
+
+void read_private_key() {
+    FILE *fptr;
+    char buffer[100];
+    int d,n;
+
+    fptr = fopen("key_priv.txt", "r");
+    if (fptr == NULL) {
+        printf("Couldn't write private key file.");
+        return;
+    }
+
+    if(fgets(buffer, sizeof(buffer), fptr) != NULL) {
+        if(sscanf(buffer, "%d,%d", &d, &n) == 2) {
+            printf("e: %d\n", d);
+            printf("n: %d\n", n);
+        } else {
+            fprintf(stderr, "Error parsing values\n");
+            fclose(fptr);
+            return;
+        }
+    } else {
+        fprintf(stderr, "Error reading file\n");
+        fclose(fptr);
+        return;
+    }
+    fclose(fptr);
+    encrypt(d, n);
+    return;
+}
+
+void read_public_key() {
+    FILE *fptr;
+    char buffer[100];
+    int e,n;
+
+    fptr = fopen("key_pub.txt", "r");
+    if (fptr == NULL) {
+        printf("Couldn't write public key file.");
+        return;
+    }
+
+    if(fgets(buffer, sizeof(buffer), fptr) != NULL) {
+        if(sscanf(buffer, "%d,%d", &e, &n) == 2) {
+            printf("e: %d\n", e);
+            printf("n: %d\n", n);
+        } else {
+            fprintf(stderr, "Error parsing values\n");
+            fclose(fptr);
+            return;
+        }
+    } else {
+        fprintf(stderr, "Error reading file\n");
+        fclose(fptr);
+        return;
+    }
+    fclose(fptr);
+    return;
 }
